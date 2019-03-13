43e62834673ed8764207d8538cf1286ccbe299f2
shiva <shiva@pgi-amarendrad1.Prodigy.local>
2019-03-13 13:44:07 +0530

    first Commit

---
+++ src/com/infinity/service/main/TimeServerPublisher.java
@@ -0,0 +1,15 @@
+package com.infinity.service.main;
+
+import javax.xml.ws.Endpoint;
+
+import com.infinity.service.TimeServerImpl;
+
+public class TimeServerPublisher {
+
+	public static void main(String[] args) {
+		System.out.println("got");
+		Endpoint.publish(" http://192.168.1.96:8088/timerService", new TimeServerImpl());
+
+	}
+
+}
